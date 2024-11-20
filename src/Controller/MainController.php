3fcad20d7f98ad859437e715f913f1e02bbfa61c
Guillaume <guillaume.rebourgeon@hotmail.fr>
2024-11-20 16:41:03 +0100

    Mise en place de la page main (page d'accueil).

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class TestController extends AbstractController
+class MainController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('test/index.html.twig', [
-            'controller_name' => 'TestController',
+        return $this->render('main/index.html.twig', [
+            'controller_name' => 'MainController',
         ]);
     }
 }
